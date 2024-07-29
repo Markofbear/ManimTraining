670e07db3129217f60c07e91bbb0b052290aa02d
Markofbear <lagerblad.bjorn@gmail.com>
2024-07-29 14:11:19 +0200

    filled with red

--- Exercises/EX_03.py
+++ Exercises/EX_03.py
@@ -3,7 +3,7 @@ from manim import *
 class Ex_03(Scene):
 
     def construct(heart):    
-        red = {"color": RED}
+        red = {"color": RED, "fill_opacity": 1, "fill_color": RED}
         l_circle = Circle(**red)
         r_circle = Circle(**red)
         square = Square(**red)
