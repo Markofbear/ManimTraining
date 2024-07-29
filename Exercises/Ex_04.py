6d7a5c8ea6f6efaeea0c8aaac664dd68871a52b3
Markofbear <lagerblad.bjorn@gmail.com>
2024-07-29 15:07:04 +0200

    start on SMALL triangle

--- Exercises/Ex_04.py
+++ Exercises/Ex_04.py
@@ -9,6 +9,11 @@ class Ex_04(Scene):
         b_triangle = Triangle(color = GREEN).scale(2.5)
         b_triangle.rotate(PI, about_point=ORIGIN)
 
+        m_triangle = b_triangle.copy()
+        m_triangle.scale(2/3, about_edge=UP).set_color(BLACK)
+
+        s_triangle = b_triangle.copy()
 
 
-        logo.add(circle, b_triangle)
+
+        logo.add(circle, b_triangle,m_triangle, s_triangle)
