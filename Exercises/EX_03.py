93a5c45ef0341f656f524de30bab044063757af6
Markofbear <lagerblad.bjorn@gmail.com>
2024-07-29 14:28:02 +0200

    exploring sizes and rotations

--- Exercises/EX_03.py
+++ Exercises/EX_03.py
@@ -4,8 +4,15 @@ class Ex_03(Scene):
 
     def construct(heart):    
         red = {"color": RED, "fill_opacity": 1, "fill_color": RED}
-        l_circle = Circle(**red)
-        r_circle = Circle(**red)
-        square = Square(**red)
 
+        l_circle = Circle(**red).set(height=2)
+        r_circle = Circle(**red).set(height=2)
+        square = Square(**red).set(height=1)
+        rotate = {"angle":-PI/4}
+
+
+        l_circle.shift(LEFT*1)
+        r_circle.shift(RIGHT*1)
+        square.shift(DOWN*1)
+        square.rotate(**rotate)
         heart.add(l_circle, r_circle, square)
