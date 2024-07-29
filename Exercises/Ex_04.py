cdc40fb2c7d58540a705de56066fd26fbf475c04
Markofbear <lagerblad.bjorn@gmail.com>
2024-07-29 15:09:15 +0200

    fixed fill on triangles Ex_04 END

--- Exercises/Ex_04.py
+++ Exercises/Ex_04.py
@@ -6,13 +6,14 @@ class Ex_04(Scene):
         fill = {"fill_opacity": 1}
         circle = Circle(color=WHITE, **fill).scale(4)
 
-        b_triangle = Triangle(color = GREEN).scale(2.5)
+        b_triangle = Triangle(color = GREEN, **fill).scale(2.5)
         b_triangle.rotate(PI, about_point=ORIGIN)
 
         m_triangle = b_triangle.copy()
         m_triangle.scale(2/3, about_edge=UP).set_color(BLACK)
 
         s_triangle = b_triangle.copy()
+        s_triangle.scale(1/3, about_edge=UP).set_color(WHITE)
 
 
 
