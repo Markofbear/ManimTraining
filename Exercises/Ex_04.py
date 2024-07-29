3bda27ba86ea79aa90e3152da91802f1bcf30a39
Markofbear <lagerblad.bjorn@gmail.com>
2024-07-29 14:53:35 +0200

    big filled circle

--- Exercises/Ex_04.py
+++ Exercises/Ex_04.py
@@ -3,4 +3,10 @@ from manim import *
 class Ex_04(Scene):
 
     def construct(logo):
-        
+        fill = {"fill_opacity": 1}
+        circle = Circle(color=WHITE, **fill).scale(4)
+
+
+
+
+        logo.add(circle)
