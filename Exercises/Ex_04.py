07922ea5ba73dc9b830bc1cd37ac36fba0c6e913
Markofbear <lagerblad.bjorn@gmail.com>
2024-07-29 14:56:38 +0200

    BIG triangle start

--- Exercises/Ex_04.py
+++ Exercises/Ex_04.py
@@ -6,7 +6,9 @@ class Ex_04(Scene):
         fill = {"fill_opacity": 1}
         circle = Circle(color=WHITE, **fill).scale(4)
 
+        b_triangle = Triangle(color = GREEN).scale(2.5)
+        b_triangle.rotate(PI, about_point=ORIGIN)
 
 
 
-        logo.add(circle)
+        logo.add(circle, b_triangle)
