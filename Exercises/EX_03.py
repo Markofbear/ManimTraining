1c0db7f2811cecb5c6f328a69736a112483912ff
Markofbear <lagerblad.bjorn@gmail.com>
2024-07-29 14:38:15 +0200

    E_03 END

--- Exercises/EX_03.py
+++ Exercises/EX_03.py
@@ -3,11 +3,11 @@ from manim import *
 class Ex_03(Scene):
 
     def construct(heart):    
-        red = {"color": RED, "fill_opacity": 1, "fill_color": RED}
+        red = {"color": RED, "fill_opacity": 0.7, "fill_color": RED}
 
-        l_circle = Circle(**red).set(height=2)
-        r_circle = Circle(**red).set(height=2)
-        square = Square(**red).set(height=1)
+        l_circle = Circle(**red).set(height=3)
+        r_circle = Circle(**red).set(height=3)
+        square = Square(**red).set(height=3)
         rotate = {"angle":-PI/4}
 
 
