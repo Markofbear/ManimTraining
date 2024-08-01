79d53398426072cfc43ca9555b0dd2a8d314426f
Markofbear <lagerblad.bjorn@gmail.com>
2024-08-01 13:41:29 +0200

    render sb_circle in the right order so it appears

--- Exercises/Ex_05.py
+++ Exercises/Ex_05.py
@@ -18,26 +18,26 @@ semi_circle = VMobject(**fig_kwargs).set_points(
 from manim import *
 
 class ex_05(Scene):
-    def construct(yinyan):
+    def construct(self):
         fill = {"fill_opacity": 1}
-        
 
         w_circle = Circle(color=WHITE).set(height=6)
-        w_circle.rotate(PI / 2)  
-        
+        w_circle.rotate(PI / 2)
+
         semi_circle = VMobject(**fill).set_points(
             w_circle.points[:int(len(w_circle.points) / 2)]
         )
 
-        u_circle = Circle(COLOR=WHITE, **fill)
-        
         sw_circle = Circle(color=BLACK, **fill).set(height=1.5)
         sw_circle.shift(1.5 * UP)
+        
         sb_circle = Circle(color=WHITE, **fill).set(height=1.5)
         sb_circle.shift(1.5 * DOWN)
+        
+        u_circle = Circle(color=WHITE, **fill).set(height=3)
+        u_circle.shift(1.5 * UP)
 
-
-
-        yinyan.add(w_circle, semi_circle, sb_circle,sw_circle)
+        # Add elements in the correct order
+        self.add(w_circle, semi_circle, u_circle, sb_circle, sw_circle)
 
 
