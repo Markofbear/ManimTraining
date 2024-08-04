532da5b19f031dbb39793dd42eb9223e2ca09b71
Markofbear <lagerblad.bjorn@gmail.com>
2024-08-04 13:55:48 +0200

    perfecting the logo

--- Exercises/Ex_06(OptionalWIFI).py
+++ Exercises/Ex_06(OptionalWIFI).py
@@ -18,12 +18,12 @@ class Ex_06(Scene):
         L5W.shift(UP * 1)
 
         rectangle1 = Rectangle(width=4.5, height=3, color=BLACK, **fill)
-        rectangle1.rotate(60 * DEGREES)
-        rectangle1.shift(UP *2, RIGHT * 3)
+        rectangle1.rotate(50 * DEGREES)
+        rectangle1.shift(UP *1.5, RIGHT * 3.1)
 
         rectangle2 = Rectangle(width=4.5, height=3, color=BLACK, **fill)
-        rectangle2.rotate(-60 * DEGREES)
-        rectangle2.shift(UP * 2, LEFT * 3)
+        rectangle2.rotate(-50 * DEGREES)
+        rectangle2.shift(UP * 1.5, LEFT * 3.1)
 
         rectangle3 = Rectangle(width=10, height=4, color=BLACK, **fill)
         rectangle3.shift(DOWN * 2)
