e9e52cd36fb08e9829b6c1d1623b297667fb12a0
Markofbear <lagerblad.bjorn@gmail.com>
2024-08-04 13:46:57 +0200

    last minute changes on WIFI

--- Exercises/Ex_06(OptionalWIFI).py
+++ Exercises/Ex_06(OptionalWIFI).py
@@ -17,13 +17,13 @@ class Ex_06(Scene):
         L5W = Circle(color=WHITE, **fill).set(height=1)
         L5W.shift(UP * 1)
 
-        rectangle1 = Rectangle(width=4, height=3, color=BLACK, **fill)
-        rectangle1.rotate(50 * DEGREES)
-        rectangle1.shift(UP + RIGHT * 3)
+        rectangle1 = Rectangle(width=4.5, height=3, color=BLACK, **fill)
+        rectangle1.rotate(60 * DEGREES)
+        rectangle1.shift(UP *2, RIGHT * 3)
 
-        rectangle2 = Rectangle(width=4, height=3, color=BLACK, **fill)
-        rectangle2.rotate(-50 * DEGREES)
-        rectangle2.shift(UP + LEFT * 3)
+        rectangle2 = Rectangle(width=4.5, height=3, color=BLUE, **fill)
+        rectangle2.rotate(-60 * DEGREES)
+        rectangle2.shift(UP * 2, LEFT * 3)
 
         rectangle3 = Rectangle(width=10, height=4, color=BLACK, **fill)
         rectangle3.shift(DOWN * 2)
