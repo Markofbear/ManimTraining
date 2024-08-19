6905ba613367a3a3283b33fd2dbda9312757a3af
Markofbear <lagerblad.bjorn@gmail.com>
2024-08-19 15:03:41 +0200

    repition

--- Exercises/CH7 Groups and VGroups/07_03_Homework.py
+++ Exercises/CH7 Groups and VGroups/07_03_Homework.py
@@ -1,25 +1,21 @@
 """Draw a fractal dragon (don't use list comprehension here)."""
 
-from manim import *
-
+from manim import * 
 
 class Dragon(Scene):
     def construct(self):
         path = VGroup()
-        first_line = Line(ORIGIN,UP / 5,stroke_width=1.2)
-        path.add(first_line)
-        iterations = 13
+        line = Line(ORIGIN, UP * 5, stroke_width=1.2)
+        path.add(line)
+        iteration = 13
 
-        for i in range(iterations):
-            new_path = path.copy()
-            new_path.rotate(
-                        90*DEGREES,
-                        about_point=path[-1].get_end() if i == 0 else path[-1].get_start()
-                    )
-            post_path = reversed([*new_path])
-            path.add(*post_path)
+        for i in range(iteration):
+            path2 = path.copy()
+            path2.rotate(90*DEGREES, about_point=path[-1].get_end() if i == 0 else path[-1].get_start())
+            pathreversed = reversed([*path2])
+            path.add(*pathreversed)
 
-        path.set(height=config.frame_height-1)
-        path.move_to(ORIGIN)
-        
-        self.add(path)
+            path.set(height=config.frame_height-1)
+            path.move_to(ORIGIN)
+
+            self.add(path)
